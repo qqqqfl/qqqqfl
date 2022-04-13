3bcc5b008e04d2acebd243f16724980620986ebf
yuzhihao <227885202@qq.com>
2022-04-13 21:33:11 +0800

    test0

--- mysql/index.js
+++ mysql/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 
 // 调用mysql的createPool方法并传入配置项
 const db = mysql.createPool({
-  host: '127.0.0.1',
+  host: 'http://127.0.0.1',
   user: 'root',
   password: 'admin123',
   database: 'vue_shop_db',
